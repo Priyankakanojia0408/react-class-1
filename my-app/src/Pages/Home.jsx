b0ea36908ad998e5011ca7cb71d74b12852c6f53
Priyanka <priyanka41994@gmail.com>
2025-12-02 17:40:19 +0100

    did coding for navbar

---
+++ my-app/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function Home() {
+    return (
+        <div>
+            <h1> Home Page</h1>
+            <p>This is the home page of the application.</p>
+        </div>
+    );
+}
+export default Home;
